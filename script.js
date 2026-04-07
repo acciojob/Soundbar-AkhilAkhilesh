fa913982dae8c9e395f285f18982836477e27a03
AkhilAkhilesh <120119311+AkhilAkhilesh@users.noreply.github.com>
2026-04-07 13:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+ const buttons = document.querySelectorAll('.btn');
+        const stopBtn = document.querySelector('.stop');
+        let audio;
+
+        buttons.forEach((button)=>{
+            button.addEventListener('click',()=>{
+                if(audio){
+                    audio.pause();
+                }
+                audio = new Audio(`https://github.com/acciojob/Soundbar-AkhilAkhilesh/blob/master/sounds/${button.innerText}.mp3?raw=true`);
+                audio.play();
+            })
+        })
+
+        stopBtn.addEventListener('click',()=>{
+            if(audio){
+                audio.pause();
+            }
+        })
